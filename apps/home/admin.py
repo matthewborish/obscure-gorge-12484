822b9193bc24d6dd16942623e1c797258cadbc6b
Matthew <matthew.borish@gmail.com>
2023-02-26 15:59:58 -0800

    updated tool db model

--- apps/home/admin.py
+++ apps/home/admin.py
@@ -3,6 +3,13 @@
 Copyright (c) 2019 - present AppSeed.us
 """
 
+
 from django.contrib import admin
+from .models import Tool
+
+
+class ToolAdmin(admin.ModelAdmin):
+    list_display = ("project_name", "project_url")
 
 # Register your models here.
+admin.site.register(Tool, ToolAdmin)
--- apps/home/models.py
+++ apps/home/models.py
@@ -2,10 +2,23 @@
 """
 Copyright (c) 2019 - present AppSeed.us
 """
-
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
+
+
+class Tool(models.Model):
+    project_name = models.CharField(max_length=100)
+    project_url = models.CharField(max_length=1000, default=None)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField(max_length=10000)
+    date_posted = models.DateTimeField(default=timezone.now)
+
+    class Meta:
+        verbose_name_plural = "Tools"
+
+    def __str__(self):
+        return self.project_name
 
 
 
